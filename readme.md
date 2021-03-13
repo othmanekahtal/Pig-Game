c04cb5d8340d273a9fb15c5b085c7b8c55c35856
othmanekahtal <othmanekahtal@gmail.com>
2021-03-13 16:56:56 +0100

    final commit 🎲

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Pig Game 🎲
+
+Pig game is simple dice roll game , it's amazing App for apply js knowledge
+
+<img src="">
+
+You can Play my game <a href="">in this link 🎲</a>
+
+**NOTE : this application is a part of Jonas Schmedtmann javascript course , believe me it's huge course check it here**
---
+++ script.js
@@ -0,0 +1,84 @@
+"use strict";
+
+// // Game Control :
+let dice = document.querySelector(".dice");
+let new_game_btn = document.querySelector(".btn--new");
+let roll_btn = document.querySelector(".btn--roll");
+let hold_btn = document.querySelector(".btn--hold");
+
+// // get Players :
+let players = document.querySelectorAll(".player");
+
+// By default setting :
+dice.style.display = "none";
+document.querySelectorAll(".score").forEach((elm) => {
+  elm.textContent = 0;
+});
+// rolling dice :
+
+function roll_event() {
+  dice.style.display = "block";
+  let random_number = Math.trunc(Math.random() * 6 + 1);
+  dice.src = `dice-${random_number}.png`;
+  console.log(random_number);
+  if (random_number === 1) {
+    document.querySelector(".player--active .current-score").textContent = 0;
+    players.forEach((elm) => {
+      if (elm.classList.contains("player--active")) {
+        elm.classList.remove("player--active");
+      } else if (!elm.classList.contains("player--active")) {
+        elm.classList.add("player--active");
+      }
+    });
+  } else {
+    players.forEach((elm) => {
+      if (elm.classList.contains("player--active")) {
+        elm.querySelector(".current-score").textContent =
+          Number(elm.querySelector(".current-score").textContent) +
+          random_number;
+      }
+    });
+  }
+}
+roll_btn.addEventListener("click", roll_event);
+
+function hold_event() {
+  document.querySelector(".player--active .score").textContent =
+    Number(document.querySelector(".player--active .score").textContent) +
+    Number(
+      document.querySelector(".player--active .current-score").textContent
+    );
+  document.querySelector(".player--active .current-score").textContent = 0;
+  if (
+    Number(document.querySelector(".player--active .score").textContent) >= 100
+  ) {
+    document.querySelector(".player--active").classList.add("player--winner");
+    dice.style.display = "none";
+    roll_btn.removeEventListener("click", roll_event);
+    hold_btn.removeEventListener("click", hold_event);
+  }
+  players.forEach((elm) => {
+    if (elm.classList.contains("player--active")) {
+      elm.classList.remove("player--active");
+    } else if (!elm.classList.contains("player--active")) {
+      elm.classList.add("player--active");
+    }
+  });
+}
+hold_btn.addEventListener("click", hold_event);
+
+// reset Game
+new_game_btn.addEventListener("click", () => {
+  roll_btn.addEventListener("click", roll_event);
+  hold_btn.addEventListener("click", hold_event);
+  console.log("object");
+  players.forEach((Element) => {
+    Element.classList.remove("player--active");
+    Element.querySelector(".current-score").textContent = 0;
+    Element.querySelector(".score").textContent = 0;
+    if (Element.classList.contains("player--winner")) {
+      Element.classList.remove("player--winner");
+    }
+  });
+  players[0].classList.add("player--active");
+});
